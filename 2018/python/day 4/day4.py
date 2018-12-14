de01e3b429f5753bfb6231b07c87d0c37f9b46f4
Rod Knowlton <codelahoma@gmail.com>
2018-12-14 13:06:50 -0600

    more 4

--- 2018/python/day 4/day4.py
+++ 2018/python/day 4/day4.py
@@ -1,3 +1,6 @@
+import re
+
+
 with open('input.txt', 'r') as f:
   slurp = f.read()
   
@@ -5,3 +8,17 @@ logs = slurp.splitlines()
 
 logs.sort()
 
+guard_pattern = 'Guard #(\d+)'
+
+logs_by_guard = {}
+
+for log in logs:
+  m = re.search(guard_pattern, log)
+  if m is not None:
+    guard = m.groups()[0]
+    if guard not in logs_by_guard.keys():
+      logs_by_guard[guard] = []
+  else:
+    logs_by_guard[guard].append(log)
+    
+sleepers = [(k, v) for (k, v) in logs_by_guard.items() if len(v) > 0]
